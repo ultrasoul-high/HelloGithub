aaa5ca1c6cbad70de98db59ba69e7ebd36d5fad7
JUN <jun.github@gmail.com>
2018-05-12 09:31:49 +0900

    First commit.

---
+++ src/hello_github/HelloGithub.java
@@ -0,0 +1,10 @@
+package hello_github;
+
+public class HelloGithub {
+
+	public static void main(String[] args) {
+		// なぜかpleiadesじゃなくてEclipseの単体インストールしてしまったので画面が英語だらけでわけわからない
+		System.out.println("Hello Github World.");
+	}
+
+}
